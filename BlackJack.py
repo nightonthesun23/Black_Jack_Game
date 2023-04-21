3847feae095133b17eab81c2251ddd284a955b93
Sarah McNeely <SarahMcNeely64@gmail.com>
2023-04-20 19:51:35 -0700

    Added function dealCard.

--- BlackJack.py
+++ BlackJack.py
@@ -4,13 +4,15 @@ import random
 deck = [2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10, 'J', 'Q','K','A','J', 'Q','K','A','J', 'Q','K','A','J', 'Q','K','A']
 playerHand = []
 dealerHand = []
-print(deck)
-
 
 # deal the cards 
-
+def dealCard(turn):
+    card = random.choice(deck)
+    turn.append(card)
+    deck.remove(card)
 # calculate the total of each hand 
 
 # check for winner
 
 # game loop 
+
