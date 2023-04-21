dd4c5530290f67f463ad69d75889ab465ee214b8
Sarah McNeely <SarahMcNeely64@gmail.com>
2023-04-20 19:49:19 -0700

    Added deck of cards / dealer hand.

--- BlackJack.py
+++ BlackJack.py
@@ -1,6 +1,11 @@
 import random
 
 # deck of cards / player dealer hand 
+deck = [2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10, 'J', 'Q','K','A','J', 'Q','K','A','J', 'Q','K','A','J', 'Q','K','A']
+playerHand = []
+dealerHand = []
+print(deck)
+
 
 # deal the cards 
 
